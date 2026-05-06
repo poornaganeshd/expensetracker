cdc389b8311a4a8300584c1ed0cf18f38b4ad886
poornaganeshd <poornaganesh.d@gmail.com>
2026-05-06 12:56:13 +0530

    update

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nomad-app-v9';
+const CACHE_NAME = 'nomad-app-motosfsp';
 const APP_SHELL = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
